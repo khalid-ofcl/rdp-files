661cedf7a5f841f03a00b4f0e6adac6452f790ba
Khalid Saifullah <105145845+khalid-ofcl@users.noreply.github.com>
2024-07-05 13:03:16 +0600

    Update login.py

--- login.py
+++ login.py
@@ -35,7 +35,7 @@ for x, y, duration in actions:
     if (x, y) in [(291, 250), (310, 338)]:
         # For "first fill" and "second fill" coordinates, type the desired text
         pag.keyDown('D')  # Press the "D" key
-        text_to_type = "Disalardp1"
+        text_to_type = "@#Disala123456"
         pag.typewrite(text_to_type)
 
 def save_echo_to_batch(file_path, echo_text):
@@ -44,7 +44,7 @@ def save_echo_to_batch(file_path, echo_text):
 
 def run_rustdesk_command():
     clipboard_text = pyperclip.paste()
-    password_echo = 'Password : Disalardp1'  
+    password_echo = 'Password : @#Disala123456'  
     save_echo_to_batch('show.bat', f'RustDesk ID: {clipboard_text}')
     save_echo_to_batch('show.bat', password_echo)
 
